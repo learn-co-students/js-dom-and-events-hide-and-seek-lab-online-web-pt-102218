90b0340f5dd0da147b87f6669734447ccd1bb82a
Arye Eidelman <aryedov.e@gmail.com>
2019-05-28 10:20:16 -0400

    Done.

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ function nestedTarget() {
 }
 
 function deepestChild() {
-
+  let child, node = document.getElementById( 'grand-node' )
+  while (child = node.children[0]) { node = child }
+  return node
 }
 
-function increaseRankBy() {
-
+function increaseRankBy(rank) {
+  Array.from(document.getElementsByClassName( 'ranked-list' ))
+    .flatMap(list => Array.from(list.children))
+    .forEach(li => li.innerHTML = (parseInt(li.innerHTML) + rank));
 }
