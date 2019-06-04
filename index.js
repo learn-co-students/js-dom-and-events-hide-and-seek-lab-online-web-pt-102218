436460740265d80a9bf9d62f800fe529281878f9
Johnathon Wood <jaywood128@gmail.com>
2019-06-04 17:52:02 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  return document.querySelectorAll('#grand-node div')[3];
+}
+function increaseRankBy( n ) {
+  const rankedLists = document.querySelectorAll( '.ranked-list' )
+
+  for ( let i = 0, l = rankedLists.length; i < l; i++ ) {
+    let children = rankedLists[ i ].children
+
+    for ( let j = 0, k = children.length; j < k; j++ ) {
+      children[ j ].innerHTML = parseInt( children[ j ].innerHTML ) + n
+    }
+  }
+} 
