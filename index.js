812c853491cecbe3697e151faa83e4ae1a996e8c
Paul Ashour <paul.ashour@gmail.com>
2019-06-21 22:39:01 -0600

    Done.

--- index.js
+++ index.js
@@ -7,10 +7,25 @@ function nestedTarget() {
 }
 
 function deepestChild() {
-  return document.querySelector("#grand-node").lastChild;
+  let list = document.querySelector("#grand-node");
+  while (list.children.length > 0) {
+    list = list.children[0]
+  }
+  return list
 }
 
 function increaseRankBy(n) {
-  let list = document.querySelector(".ranked-list");
+  let list = document.querySelectorAll(".ranked-list");
+  for (let index = 0; index < list.length; index++) {
+    let ul = list[index]
+    console.log(ul)
+    for (let i = 0; i < ul.children.length; i++) {
+      let li = ul.children[i];
+      console.log(li.textContent)
+      li.textContent = parseInt(li.textContent) + n;
+      
+    }
+    
+  }
   return list;
 }
